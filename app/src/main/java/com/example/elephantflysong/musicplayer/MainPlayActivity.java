84cc6d96710348b2240f4ba2017dcaedb0e8739e
qustacmersxf <1725538564@qq.com>
2018-06-12 16:24:45 +0800

    First commit

---
+++ app/src/main/java/com/example/elephantflysong/musicplayer/MainPlayActivity.java
@@ -0,0 +1,13 @@
+package com.example.elephantflysong.musicplayer;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainPlayActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main);
+    }
+}
